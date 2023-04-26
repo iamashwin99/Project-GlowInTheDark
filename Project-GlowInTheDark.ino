935f36e89b43af1555bffab75134213183e97e63
iamashwin99 <ashwin-kumar.karnad@mpsd.mpg.de>
2023-04-26 13:24:41 +0200

    add two patterns

--- Project-GlowInTheDark.ino
+++ Project-GlowInTheDark.ino
@@ -22,6 +22,7 @@ Adafruit_DotStar strip =
 int16_t head = 0;    // Index of first 'on' pixel
 int16_t tail = -10;  // Index of first 'off' pixel
 uint32_t color = 0xFF0000;  // 'On' color (starts red)
+uint8_t leds_to_skip = 0;   // Number of LEDs to skip between each LED
 
 // --- Public function definitions ---
 
@@ -78,21 +79,53 @@ void loop()
 
 static void handleLedStrip()
 {
+  rainbow(20) 
+}
+
+static void strandedPattern(int wait)
+{
   strip.setPixelColor(head, color);
   strip.setPixelColor(tail, 0);
   strip.show();
-  delay(20);
-
-  if (++head >= NUMPIXELS)
+  delay(wait);
+  if (leds_to_skip > 0)
   {
-    head = 0;
-    if ((color >>= 8) == 0)
-      color = 0xFF0000;
+    for (uint8_t i = 0; i < leds_to_skip; i++)
+    {
+      if (++head >= NUMPIXELS)
+        head = 0;
+      if (++tail >= NUMPIXELS)
+        tail = 0;
+    }
   }
-  if (++tail >= NUMPIXELS)
-    tail = 0;
+  if ((color >>= 8) == 0)
+  color = 0xFF0000;
 }
 
+// Rainbow cycle along whole strip. Pass delay time (in ms) between frames.
+// From https://github.com/adafruit/Adafruit_DotStar/blob/master/examples/onboard/onboard.ino
+void rainbow(int wait) {
+  // Hue of first pixel runs 5 complete loops through the color wheel.
+  // Color wheel has a range of 65536 but it's OK if we roll over, so
+  // just count from 0 to 5*65536. Adding 256 to firstPixelHue each time
+  // means we'll make 5*65536/256 = 1280 passes through this outer loop:
+  for(long firstPixelHue = 0; firstPixelHue < 5*65536; firstPixelHue += 256) {
+    for(int i=0; i<strip.numPixels(); i=i+leds_to_skip) { // For each pixel in strip...
+      // Offset pixel hue by an amount to make one full revolution of the
+      // color wheel (range of 65536) along the length of the strip
+      // (strip.numPixels() steps):
+      int pixelHue = firstPixelHue + (i * 65536L / strip.numPixels());
+      // strip.ColorHSV() can take 1 or 3 arguments: a hue (0 to 65535) or
+      // optionally add saturation and value (brightness) (each 0 to 255).
+      // Here we're using just the single-argument hue variant. The result
+      // is passed through strip.gamma32() to provide 'truer' colors
+      // before assigning to each pixel:
+      strip.setPixelColor(i, strip.gamma32(strip.ColorHSV(pixelHue)));
+    }
+    strip.show(); // Update strip with new contents
+    delay(wait);  // Pause for a moment
+  }
+}
 
 
 // convert the file extension to the MIME type
