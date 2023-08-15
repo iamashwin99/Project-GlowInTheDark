e7200f458719511bbbd4b5652ac9be39df64a255
iamashwin99 <ashwin-kumar.karnad@mpsd.mpg.de>
2023-08-15 14:52:41 +0200

    add ip configs

--- Config.h
+++ Config.h
@@ -1,6 +1,6 @@
 #ifndef CONFIG_H_
 #define CONFIG_H_
-
+#include <ESP8266WiFi.h>
 
 // --- Includes ---
 #include <Arduino.h>
@@ -15,6 +15,11 @@ const uint8_t LED_PIN = 0;
 
 const uint8_t DOTSTAR_DATA_PIN = 4;
 const uint8_t DOTSTAR_CLOCK_PIN = 5;
+
+IPAddress local_IP(192,168,4,22);
+IPAddress gateway(192,168,4,9);
+IPAddress subnet(255,255,255,0);
+
 
 
 #endif /* CONFIG_H_ */
